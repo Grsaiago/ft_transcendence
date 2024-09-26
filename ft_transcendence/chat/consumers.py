64d7671f4b59b65b3304a2c044ee0a688c099efa
Gabriel Saiago <grsaiago@gmail.com>
2024-09-26 18:38:49 -0300

    perf(chat): Add types and more info to chat_message event

--- ft_transcendence/chat/consumers.py
+++ ft_transcendence/chat/consumers.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, TypedDict, Union
 import json
 from django.db.models import Q
 import user_management.models as social_models
@@ -6,6 +6,11 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.layers import InMemoryChannelLayer
 from channels_redis.core import RedisChannelLayer
 
+class ChatMessage(TypedDict):
+    type: str
+    chat_id: str
+    sender: str
+    message: str
 
 class ChatConsumer(AsyncWebsocketConsumer):
     groups: List[str] = ['general_chat']
@@ -14,6 +19,8 @@ class ChatConsumer(AsyncWebsocketConsumer):
     dynamic_groups: List[str] = []
     # add some types to make development more sane
     channel_layer: Union[InMemoryChannelLayer, RedisChannelLayer]
+    scope: dict
+
 
     async def connect(self):
         groups_to_add = social_models.Friendship.objects.filter(
@@ -25,9 +32,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
             await self.add_to_group(group)
         await self.accept()
 
+
     async def disconnect(self, code):
         for group in self.dynamic_groups:
             await self.remove_from_group(group)
+
 
     # aqui é quando o servidor recebe uma mensagem qualquer de um websocket
     # e aqui ele só dá um broadcast nessa mensagem pro chat target.
@@ -37,25 +46,31 @@ class ChatConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(str(text_data))
         message = text_data_json["message"]
         chat_id = text_data_json["chat_id"]
+        sender = self.scope['user'].username
 
-        # TODO: Talvez fazer assim pra no chat_message
-        # pra gente conseguir enviar a chave de "chat_id" pro front
-        await self.channel_layer.group_send(chat_id, {"type": "chat.message", "chat_id": chat_id, "message": message})
+        # TODO: fazer assim pra no chat_message a gente conseguir enviar
+        # a chave de "chat_id" pro front
+        content: ChatMessage = {
+            "type": "chat.message",
+            "chat_id": chat_id,
+            "sender": sender,
+            "message": message
+        }
+        await self.channel_layer.group_send(chat_id, content)
+
 
     # aqui é quando o websocket recebe um evento do tipo chat.message no back
-    async def chat_message(self, event):
-        message = event['message']
-        chat_id = event['chat_id']
-
+    async def chat_message(self, event: ChatMessage):
         # TODO: Acho que tem que mandar mais contexto nesse frame.
         # de repente mandar uma chave "chat_id", tipo assim:
-        await self.send(text_data=json.dumps({"chat_id": chat_id, "message": message}))
+        await self.send(text_data=json.dumps(event))
 
 
-    async def remove_from_group(self, group_id):
+    async def remove_from_group(self, group_id: str):
         await self.channel_layer.group_discard(group_id, self.channel_name)
         self.dynamic_groups.remove(group_id)
 
-    async def add_to_group(self, group_id):
+
+    async def add_to_group(self, group_id: str):
         await self.channel_layer.group_add(group_id, self.channel_name)
         self.dynamic_groups.append(group_id)
