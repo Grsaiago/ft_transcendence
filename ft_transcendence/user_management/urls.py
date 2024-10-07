8061ab4dc9f068b0d707914007582da5e55c453a
Gabriel Saiago <grsaiago@gmail.com>
2024-10-07 15:43:00 -0300

    Develop (#11)
    * Add: Configs for compose and postgres
    
    * Add: FriendRequestForm Model form
    
    * Add: Friendship models
    
    * Add: friendship add view
    
    * Add: friend add html
    
    * Fix: send friend request functionality and org
    
    * Add: first functional wip of friendship flow
    
    * Fix: users sending request to each other
    
    * Add: Refuse friend request func
    
    * Add: Handle race condition in request processing
    
    * Add: Remove friendships
    
    * Add: Block user capabilities
    
    * chore: add linter configuration with pre-commit, black, isort, flack8
    
    * chore: configure format and linter for django template, djlint
    
    * chore: update .pre-commit-config.yaml after pre-commit run
    
    * docs: update README with pre-commit instructions
    
    * make tests after merge. Update poetry.lock
    
    * resolving conflicts with pre-commit
    
    ---------
    
    Co-authored-by: Iza Cabral <izabelecabral@hotmail.com>
    Co-authored-by: Izabele Cabral <107276428+izacabral@users.noreply.github.com>

---
+++ ft_transcendence/user_management/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from .views import (
+    UserChangePasswordView,
+    UserFriendListView,
+    UserLoginView,
+    UserLogoutView,
+    UserRegisterView,
+)
+
+app_name = "user_management"
+
+urlpatterns = [
+    path("register", UserRegisterView.as_view(), name="register"),
+    path("login", UserLoginView.as_view(), name="login"),
+    path("logout", UserLogoutView.as_view(), name="logout"),
+    path("change_password", UserChangePasswordView.as_view(), name="change_password"),
+    path("friend_list", UserFriendListView.as_view(), name="friend_list"),
+]
---
+++ ft_transcendence/user_management/views.py
@@ -0,0 +1,74 @@
+from django.contrib import messages
+from django.contrib.auth import forms as auth_forms
+from django.contrib.auth import mixins as auth_mixins
+from django.contrib.auth import views as auth_views
+from django.db.models import Q
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic as generic_views
+
+from .forms import BlockUserForm, FriendRequestForm, TranscendenceUserCreationForm
+from .models import FriendRequest, Friendship
+
+
+class UserRegisterView(generic_views.FormView):
+    template_name = "user_management/register.html"
+    form_class = TranscendenceUserCreationForm
+    # TODO: Change to homepage instead of password change page
+    success_url = reverse_lazy("user_management:friend_list")
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+
+class UserLoginView(auth_views.LoginView):
+    template_name = "user_management/login.html"
+    redirect_authenticated_user = True
+    # TODO: Change to homepage instead of password change page
+    success_url = reverse_lazy("user_management:friend_list")
+
+    def form_invalid(self, form):
+        messages.error(self.request, "invalid username or password")
+        return self.render_to_response(self.get_context_data(form=form))
+
+
+class UserLogoutView(auth_views.LogoutView):
+    next_page = reverse_lazy("user_management:login")
+
+
+class UserChangePasswordView(
+    auth_mixins.LoginRequiredMixin, auth_views.PasswordChangeView
+):
+    template_name = "user_management/change_password.html"
+    form_class = auth_forms.PasswordChangeForm
+    success_url = reverse_lazy("user_management:friend_list")
+
+
+class UserFriendListView(auth_mixins.LoginRequiredMixin, generic_views.View):
+    template_name = "user_management/friend_list.html"
+
+    def get(self, request, *args, **kwargs):
+        # O form pra mandar um invite pra um usuário
+        friend_request_form = FriendRequestForm()
+        block_user_form = BlockUserForm()
+        pending_friend_requests = FriendRequest.objects.filter(receiver=request.user)
+        sent_friend_requests = FriendRequest.objects.filter(sender=request.user)
+        # essas duas variáveis abaixo são pra filtrar o resultado da query
+        # de entradas na tabela de amizade
+        friends = Friendship.objects.filter(
+            Q(first_user=request.user.id) | Q(second_user=request.user.id)
+        )
+        current_friends = [
+            entry.first_user if entry.first_user != request.user else entry.second_user
+            for entry in friends
+        ]
+
+        context = {
+            "block_user_form": block_user_form,
+            "friend_request_form": friend_request_form,
+            "pending_friend_requests": pending_friend_requests,
+            "sent_friend_requests": sent_friend_requests,
+            "current_friends": current_friends,
+        }
+        return render(request, self.template_name, context)
