fcbe02aa63bf3d8961325e40c43c906262c76edb
Iza Cabral <izabelecabral@hotmail.com>
2024-10-21 15:05:50 -0300

    wip: create a PongGlass to front and refactor pong(handle-pong)

---
+++ ft_transcendence/pong/static/pong/js/PongGame.js
@@ -0,0 +1,58 @@
+const TABLE_COLOR = 'black';
+const LINE_COLOR = 'gray';
+const BALL_COLOR = LINE_COLOR;
+const PADDLE_COLOR = LINE_COLOR;
+
+export class PongGame {
+    constructor(context, canvasWidth, canvasHeight) {
+        this.context = context;
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+    }
+
+    clearCanvas() {
+        this.context.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+        this.context.fillStyle = TABLE_COLOR
+        this.context.fillRect(0, 0, this.canvasWidth, this.canvasHeight);
+    }
+
+    drawHorizontalLine(y, thickness) {
+        this.context.fillRect(0, y, this.canvasWidth, thickness);
+    }
+
+    drawVerticalDashLine(x, thickness) {
+        this.context.beginPath();
+        this.context.setLineDash([thickness, thickness]);
+        this.context.moveTo(x, 0);
+        this.context.lineTo(x, this.canvasHeight);
+        this.context.lineWidth = thickness;
+        this.context.strokeStyle = LINE_COLOR;
+        this.context.stroke();
+        this.context.setLineDash([]);
+    }
+
+    drawTable(thickness) {
+        this.context.fillStyle = LINE_COLOR;
+        this.drawHorizontalLine(0, thickness);
+        this.drawHorizontalLine(this.canvasHeight - thickness, thickness);
+        this.drawVerticalDashLine(this.canvasWidth / 2, thickness);
+    }
+
+    drawBall(ball) {
+        this.context.fillStyle = BALL_COLOR;
+        this.context.fillRect(ball.x, ball.y, ball.size, ball.size);
+    }
+
+    drawPaddle(paddle) {
+        this.context.fillStyle = PADDLE_COLOR;
+        this.context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+    }
+
+    drawGameState(gameState) {
+        this.clearCanvas();
+        this.drawTable(gameState.ball.size);
+        this.drawBall(gameState.ball);
+        this.drawPaddle(gameState.paddle_left);
+        this.drawPaddle(gameState.paddle_right);
+    }
+}
---
+++ ft_transcendence/pong/static/pong/js/handle-pong.js
@@ -0,0 +1,49 @@
+import { PongGame } from './PongGame.js';
+
+//Get canvas and context
+const canvas = document.getElementById('pongCanvas');
+const context = canvas.getContext('2d');
+
+//Initialize game
+const pongGame = new PongGame(context, canvas.width, canvas.height);
+
+//Get game data
+const gameData = document.getElementById('game-data');
+const room_id = gameData.dataset.roomId
+
+console.log('room_id:', room_id);
+
+//Websocket connection
+const socket = new WebSocket('ws://' + window.location.host + '/ws/pong/');
+
+socket.onopen = function (e) {
+    const message = JSON.stringify({
+        type: "join_room",
+        room_id: room_id,
+        width: canvas.width,
+        height: canvas.height
+    });
+    console.log('Sending message:', message);
+    socket.send(message);
+    console.log('WebSocket connection established');
+}
+
+socket.onmessage = function (e) {
+    const data = JSON.parse(e.data);
+    console.log("Message from server: ", data);
+    console.log("data type: ", data.type);
+
+    if (data.type === "game_init") {
+        pongGame.drawGameState(data.game_state);
+    }
+
+    if (data.type === "update_game_state") {
+        pongGame.drawGameState(data.game_state);
+    }
+
+
+}
+
+socket.onclose = function (e) {
+    console.log('WebSocket connection closed');
+}
