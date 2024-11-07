97552c0a40c7c599febb71cb9f71afc1a1d5bd71
mari-aragao <mariazevedoaragao@gmail.com>
2024-11-07 16:43:30 -0300

    add change password route in js

---
+++ ft_transcendence/static/js/app/views/change_password.js
@@ -0,0 +1,26 @@
+
+import AbstractView from "./abstractView.js";
+
+export default class Change_password extends AbstractView {
+    constructor() {
+        super();
+        this.setTitle("change_password");
+    }
+
+    async getHtml() {
+        try {
+            const response = await fetch('/change_password/', {
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            });
+            const html = await response.text();
+            console.log(html);
+            return html;
+        }
+        catch(error) {
+            console.error('Failed to fetch page: ', error);
+            return "<p>Error loading login page</p>";
+        }
+    }
+}
