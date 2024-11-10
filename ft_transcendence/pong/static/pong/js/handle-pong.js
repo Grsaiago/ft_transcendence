7554cf67ce09fe0e63f6d61cfce7e3109a6cb3fc
Iza Cabral <izabelecabral@hotmail.com>
2024-11-10 13:17:27 -0300

    wip: replace game start from pressing Enter to Star button

--- ft_transcendence/pong/static/pong/js/handle-pong.js
+++ ft_transcendence/pong/static/pong/js/handle-pong.js
@@ -1,88 +1,97 @@
-import { PongGame } from './PongGame.js';
+import { PongGame } from "./PongGame.js";
 
 //Get canvas and context
-const canvas = document.getElementById('pongCanvas');
-const context = canvas.getContext('2d');
+const canvas = document.getElementById("pongCanvas");
+const context = canvas.getContext("2d");
 
 //Initialize game
 const pongGame = new PongGame(context, canvas.width, canvas.height);
 
 //Get game data
-const gameData = document.getElementById('game-data');
-const room_id = gameData.dataset.roomId
+const gameData = document.getElementById("game-data");
+const room_id = gameData.dataset.roomId;
 
-console.log('room_id:', room_id);
+console.log("room_id:", room_id);
 
 //Websocket connection
-const socket = new WebSocket('ws://' + window.location.host + '/ws/pong/');
+const socket = new WebSocket("ws://" + window.location.host + "/ws/pong/");
 
 socket.onopen = function (e) {
-    const message = JSON.stringify({
-        type: "join_room",
-        room_id: room_id,
-        width: canvas.width,
-        height: canvas.height
-    });
-    //console.log('Sending message:', message);
-    socket.send(message);
-    console.log('WebSocket connection established');
-}
+  const message = JSON.stringify({
+    type: "join_room",
+    room_id: room_id,
+    width: canvas.width,
+    height: canvas.height,
+  });
+  //console.log('Sending message:', message);
+  socket.send(message);
+  console.log("WebSocket connection established");
+};
 
 socket.onmessage = function (e) {
-    const data = JSON.parse(e.data);
-    //console.log("Message from server: ", data);
-    //console.log("data type: ", data.type);
+  const data = JSON.parse(e.data);
+  //console.log("Message from server: ", data);
+  //console.log("data type: ", data.type);
 
-    if (data.type === "game_init") {
-        pongGame.drawGameState(data.game_state);
-    }
+  if (data.type === "game_init") {
+    pongGame.drawGameState(data.game_state);
+  }
 
-    if (data.type === "update_game_state") {
-        pongGame.drawGameState(data.game_state);
-    }
-
-}
+  if (data.type === "update_game_state") {
+    pongGame.drawGameState(data.game_state);
+  }
+};
 
 socket.onclose = function (e) {
-    console.log('WebSocket connection closed');
-}
+  console.log("WebSocket connection closed");
+};
 
-
-// key start event listener
-document.addEventListener('keydown', function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        const message = JSON.stringify({
-            type: "start_game",
-        });
-        console.log('Sending message:', message);
-        socket.send(message);
-    }
+// button start game event listener
+const startButton = document.getElementById("startGame");
+startButton.addEventListener("click", function () {
+  const message = JSON.stringify({
+    type: "start_game",
+  });
+  console.log("Sending message:", message);
+  socket.send(message);
 });
 
 // keydown event listener
-document.addEventListener('keydown', function (event) {
-    if (event.key === "ArrowUp" || event.key === "ArrowDown" || event.key === "w" || event.key === "s" || event.key === "W" || event.key === "S") {
-        event.preventDefault();
-        const message = JSON.stringify({
-            type: "keydown",
-            key: event.key.toLocaleLowerCase(),
-        });
-        //console.log('Sending message:', message);
-        socket.send(message);
-    }
+document.addEventListener("keydown", function (event) {
+  if (
+    event.key === "ArrowUp" ||
+    event.key === "ArrowDown" ||
+    event.key === "w" ||
+    event.key === "s" ||
+    event.key === "W" ||
+    event.key === "S"
+  ) {
+    event.preventDefault();
+    const message = JSON.stringify({
+      type: "keydown",
+      key: event.key.toLocaleLowerCase(),
+    });
+    //console.log('Sending message:', message);
+    socket.send(message);
+  }
 });
 
-
 // keyup event listener
-document.addEventListener('keyup', function (event) {
-    if (event.key === "ArrowUp" || event.key === "ArrowDown" || event.key === "w" || event.key === "s" || event.key === "W" || event.key === "S") {
-        event.preventDefault();
-        const message = JSON.stringify({
-            type: "keyup",
-            key: event.key.toLocaleLowerCase(),
-        });
-        //console.log('Sending message:', message);
-        socket.send(message);
-    }
+document.addEventListener("keyup", function (event) {
+  if (
+    event.key === "ArrowUp" ||
+    event.key === "ArrowDown" ||
+    event.key === "w" ||
+    event.key === "s" ||
+    event.key === "W" ||
+    event.key === "S"
+  ) {
+    event.preventDefault();
+    const message = JSON.stringify({
+      type: "keyup",
+      key: event.key.toLocaleLowerCase(),
+    });
+    //console.log('Sending message:', message);
+    socket.send(message);
+  }
 });
