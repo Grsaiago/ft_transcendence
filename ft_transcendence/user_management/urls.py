179e1df7f2bf990e51cfb70fed770607f3c54713
Gabriel Saiago <grsaiago@gmail.com>
2024-12-20 16:38:51 -0300

    feat(user_profile_picture): add profile picture support (#49)

--- ft_transcendence/user_management/urls.py
+++ ft_transcendence/user_management/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from .views import (
     UserChangePasswordView,
@@ -22,4 +24,4 @@ urlpatterns = [
     path("friend_list/", UserFriendListView.as_view(), name="friend_list"),
     path("profile/", UserProfileView.as_view(), name="profile"),
     path("chat/", UserChatView.as_view(), name="chat"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
