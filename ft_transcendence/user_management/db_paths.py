7d99043dcb71b8dee2e8d6207be495eb7a5f60c9
Gabriel Saiago <grsaiago@gmail.com>
2024-12-23 16:14:28 -0300

    fix(user_management): trimm out a print

--- ft_transcendence/user_management/db_paths.py
+++ ft_transcendence/user_management/db_paths.py
@@ -307,7 +307,6 @@ def get_user_details(request, user_id):
 @login_required
 def update_user(request: HttpRequest):
     post_data = request.POST.copy()
-    print(post_data)
     update_user_form =  TranscendenceUserUpdateForm(post_data, request.FILES, instance=request.user)
     if update_user_form.is_valid():
         update_user_form.save()
