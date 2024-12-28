4d232f979eae83f886e52459282d222e616a1f9c
Gabriel Saiago <grsaiago@gmail.com>
2024-12-28 16:54:42 -0300

    chore(devops): Allow all hosts and make debug configurable via env

--- ft_transcendence/ft_transcendence/settings.py
+++ ft_transcendence/ft_transcendence/settings.py
@@ -28,10 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-(9rdnj=ey@s4$gzno%968@$yi4+@0wadwzp#r%9z5&%n2r1+q9"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() in ('true', '1', 't', 'yes')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
