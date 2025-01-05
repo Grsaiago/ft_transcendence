e36125a55a7646558f89018c3cc24ade9aa0c759
Gabriel Saiago <grsaiago@gmail.com>
2025-01-05 19:24:53 -0300

    fix(devops) delete prod_static_file from source.
    The fact that this folder was copied into the container prevented it
    from recompiling the static files on the 'compilestatic' entrypoint
    directive

--- ft_transcendence/prod_static_serve/js/app/providers/userInfoProvider.js
+++
@@ -1,7 +0,0 @@
-export default class UserInfoProvider {
-    constructor() {}
-
-    async getAllUsers() {} //endpoint to get all users
-
-    async getUserFriends() {} //endpoint to get user friends
-}
